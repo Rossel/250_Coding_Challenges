83e586231b878c1746c14e776122ef3a09e020ae
dbro-dev <64974724+dbro-dev@users.noreply.github.com>
2020-06-12 10:21:02 +0200

    Solution to Challenge 179

---
+++ chal179.py
@@ -0,0 +1,9 @@
+def my_func(x):
+    my_new_list = []
+    for i in x:
+        if i > 4:
+            my_new_list.append(i ** 2)
+    return my_new_list
+     
+result = my_func((2, 3, 5, 6, 4, 8, 9))
+print(result)
