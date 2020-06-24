2658bc2f3639e308bb3b8fddaf637c8ed3f7c1bb
dbro-dev <64974724+dbro-dev@users.noreply.github.com>
2020-06-24 08:47:53 +0200

    Solution to Challenge 230

---
+++ chal230.py
@@ -0,0 +1,8 @@
+import re
+     
+s = "Bitcoin was born on Jan 3rd 2009 as an alternative to the failure of the current financial system. In 2017, the price of 1 BTC reached $20000, with a market cap of over $300B. Bitcoin, Market Cap: $184,073,529,068, Price: $10,259.02, Volume 24h: $15,670,986,269, Circulating Supply: 17,942,600 BTC, Change 24h: 0.10%"
+     
+result = re.sub(r"[A-Z]\w{1,}|[6-9]", "W", s)
+     
+print(result)
+
